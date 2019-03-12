f529e7063ce859b179be41633d0f32d925c4d76a
Sundeep Pidugu <pidugusundeep5@gmail.com>
2019-03-12 19:56:02 +0530

    change app directory
    Fixed minor bugs

---
+++ my_bmi/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <firebase_admob/FirebaseAdMobPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseAdMobPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAdMobPlugin"]];
+}
+
+@end
